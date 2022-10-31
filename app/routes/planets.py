2a64ca76e6899e365b1bdcae135203d0dd1ce4d9
Jessica <jessicaambrizmadrigal@gmail.com>
2022-10-31 13:44:11 -0700

    Wave 4 updated

--- app/routes/planets.py
+++ app/routes/planets.py
@@ -1,6 +1,10 @@
+from email import message
+from urllib import response
 from flask import Blueprint,jsonify, abort, make_response, request
 from app import db
 from app.models.planets import Planet
+
+
 
 # def validate_planet(planet_id):
 #     try:
@@ -14,6 +18,20 @@ from app.models.planets import Planet
 #     abort(make_response({"message": f"planet {planet_id} not found."}, 404))
 
 planet_bp = Blueprint("planet_bp",__name__, url_prefix="/planet")
+
+def get_one_planet_or_abort(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        response_str = f"invalid planet_id: `{planet_id}`.Id must be an integer"
+        abort(make_response(jsonify({'message':response_str}), 400))
+    matching_planet = Planet.query.get(planet_id)
+    if not matching_planet: 
+        response_str = f"Planet with id {planet_id} was not found in the database"
+        abort(make_response(jsonify({'message':response_str}), 400))
+    return matching_planet
+
+
 @planet_bp.route("", methods=["POST"])
 def add_planet():
     request_body = request.get_json()
@@ -25,7 +43,6 @@ def add_planet():
     db.session.add(new_planet)
     db.session.commit()
     return {"id":new_planet.id}, 201
-
 
 
 @planet_bp.route("", methods=["GET"])
@@ -42,13 +59,37 @@ def get_all_planets():
         response.append(planet_dict)
     return jsonify(response), 200
 
-# @planet_bp.route("/<planet_id>", methods=["GET"])
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
-    
-#     return jsonify({
-#         "id" : planet.id,
-#         "name" : planet.name,
-#         "planet description" : planet.description,
-#         "type" : planet.type
-#     })
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    chosen_planet = get_one_planet_or_abort(planet_id)
+    planet_dict = {
+            "id" : chosen_planet.id,
+            "name" : chosen_planet.name,
+            "planet description" : chosen_planet.description,
+            "type" : chosen_planet.type
+        }
+    return jsonify(planet_dict), 200
+
+@planet_bp.route("/<planet_id>", methods=["PUT"])
+def update_one_planet(planet_id):
+    chosen_planet = get_one_planet_or_abort(planet_id)
+    request_body = request.get_json()
+    if "name" not in request_body or \
+        "description" not in request_body or \
+        "type" not in request_body:
+            return jsonify({"message": "request must include name, description, type "}), 400
+    chosen_planet.name = request_body["name"]
+    chosen_planet.description = request_body["description"]
+    chosen_planet.type = request_body["type"]
+
+    db.session.commit()
+    return jsonify({"message": f"successfully replaced planet with id{planet_id}"}), 200
+
+
+@planet_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_one_planet(planet_id):
+    chosen_planet = get_one_planet_or_abort(planet_id)
+    db.session.delete(chosen_planet)
+    db.session.commit()
+    return jsonify({"message": f"successfully deleted planet with id{planet_id}"}), 200
+
